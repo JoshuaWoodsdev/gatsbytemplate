b11097756d1c02b22a71609e4c38afff48af51b1
Joshua Woods <nordev@protonmail.com>
2023-06-28 12:56:53 -0500

    setup of basic folders

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `gatsbytemplate`,
+    description: `gatsbytemplate template you can use to base your gatsby projects on. includes a simple layour and folder setup`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
